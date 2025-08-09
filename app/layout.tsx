8e22e4406ef71207aad37d0d8c31a168cf2c3ee5
nnlgsakib <nlgarts@gmail.com>
2025-08-09 12:54:57 +0600

    added crosshair

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,14 @@
 import type React from "react"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import Crosshair from "@/components/Crosshair";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="min-h-screen bg-background text-foreground antialiased">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Crosshair />
           {children}
         </ThemeProvider>
       </body>
